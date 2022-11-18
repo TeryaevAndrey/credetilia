8e5d0253ec26f918203a35db676ca0a278185006
Andrey <teryaev.2004@mail.ru>
2022-11-18 18:10:49 +0300

    add-blog-and-callback-and-start-footer

--- src/scripts/sliders.js
+++ src/scripts/sliders.js
@@ -2,12 +2,13 @@ import Swiper, {Pagination} from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 
-const reviewsSlider = new Swiper(".swiper", {
+const swiper = new Swiper(".swiper", {
   loop: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true
   },
   slidesPerView: 1.3,
-  modules: [Pagination]
+  modules: [Pagination],
+  spaceBetween: 15
 });
