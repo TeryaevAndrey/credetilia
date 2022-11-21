a5f4feea0ef8edb654083a98f719f9f4250a29a8
Andrey <teryaev.2004@mail.ru>
2022-11-21 23:03:13 +0300

    fix-bug-inputs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import "./css/index.css";
+import "./scripts/menu";
 import "./scripts/sliders";
 import "./scripts/range";
-import "./scripts/menu";
--- src/scripts/range.js
+++ src/scripts/range.js
@@ -7,6 +7,8 @@ const rangeMesesValue = document.getElementById("rangeMesesValue");
 const rangeMesesInput = document.getElementById("rangeMesesInput");
 const monthlyFee = document.getElementById("monthlyFee");
 const totalDue = document.getElementById("totalDue");
+const fastRangeValue = document.getElementById("fastRangeValue");
+const fastRangeInput = document.getElementById("fastRangeInput");
 
 const calcular = (duration, amount, tin, tae) => {
   var meses = duration;
@@ -34,16 +36,22 @@ const calcular = (duration, amount, tin, tae) => {
 rangeValueMin.textContent = rangeInput.getAttribute("min");
 rangeValueMax.textContent = rangeInput.getAttribute("max");
 
+fastRangeInput.addEventListener("input", (e) => {
+  fastRangeValue.value = parseInt(e.target.value);
+});
+
+fastRangeValue.addEventListener("input", (e) => {
+  fastRangeInput.value = parseInt(e.target.value);
+});
+
 rangeInput.addEventListener("input", (e) => {
-  rangeValue.value = e.target.value + "€";
+  rangeValue.value = e.target.value;
   calcular(rangeMesesInput.value, rangeInput.value, 9, 9);
 });
 
 rangeValue.addEventListener("input", (e) => {
   rangeInput.value = parseInt(e.target.value);
-
-  const value = parseInt(e.target.value) || 1;
-  e.target.value = value + "€";
+  calcular(rangeMesesInput.value, rangeInput.value, 9, 9);
 });
 
 rangeMesesInput.addEventListener("input", (e) => {
@@ -53,9 +61,7 @@ rangeMesesInput.addEventListener("input", (e) => {
 
 rangeMesesValue.addEventListener("input", (e) => {
   rangeMesesInput.value = parseInt(e.target.value);
-
-  const value = e.target.value;
-  e.target.value = value;
+  calcular(rangeMesesInput.value, rangeInput.value, 9, 9);
 });
 
 submitRange.addEventListener("click", (e) => {
